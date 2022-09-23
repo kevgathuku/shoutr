c919f906180b848e9edbe675591c494b2e671665
Kevin Gathuku <kevgathuku@gmail.com>
2022-09-23 08:33:55 +0300

    Run content validations when creating a new shout

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -3,6 +3,7 @@ class Shout < ApplicationRecord
   belongs_to :content, polymorphic: true
 
   validates :user, presence: true
+  validates_associated :content
 
   default_scope { order(created_at: :desc) }
 
